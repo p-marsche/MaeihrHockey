1b9906185d859cbd74fede5bf725091373206cde
Pascal Marsche <pmarsche.mmt-b2024@fh-salzburg.ac.at>
2026-02-02 16:31:15 +0100

    created goal event

---
+++ vs2022/GoalEvent.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+#include "GoalEvent.hpp"
+
+constexpr mmt_gd::IEvent::EventType mmt_gd::GoalEvent::Type = 0x94b0c800;
---
+++ vs2022/GoalEvent.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "GameObjectManager.hpp"
+#include "IEvent.hpp"
+
+namespace mmt_gd
+{
+    // int -> playerID, so we know which side it is
+    // player 1 scores -> data = 2
+    class GoalEvent final : public BasicDataEvent<int>
+    {
+    public:
+        using BasicDataEvent::BasicDataEvent;
+        
+        static const EventType Type;
+
+        EventType getEventType() override
+        {
+            return Type;
+        }
+} // namespace mmt_gd
+
