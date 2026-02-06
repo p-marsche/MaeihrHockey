314686bf9ec8484a6be5932d81144842ae412e1e
Pascal Roland Veselka <fhs52324@fh-salzburg.ac.at>
2026-02-06 11:19:20 +0100

    added files for PaddleSetupState (not functional yet, but i need to merge other branch in here, i need the config stuff)

---
+++ src/PaddleSetupState.cpp
@@ -0,0 +1,109 @@
+#include "stdafx.h"
+
+#include "PaddleSetupState.hpp"
+
+#include "Game.hpp"
+#include "GameStateManager.hpp"
+#include "InputManager.hpp"
+
+namespace mmt_gd
+{
+using namespace std;
+
+void PaddleSetupState::init()
+{
+    PROFILE_FUNCTION();
+    //m_guiGroup->setVisible(true);
+
+    if (m_isInit)
+    {
+        return;
+    }
+
+    m_view = m_game->getWindow().getView();
+
+    m_isInit = true;
+
+    // LoadGui
+    auto        guiGroup = tgui::Group::create();
+    guiGroup->loadWidgetsFromFile(Config::guiPath + "main_menu.txt");
+    m_guiGroups.emplace("MainMenu", guiGroup);
+    m_gui->add(guiGroup);
+    m_guiGroups.at("MainMenu")->setVisible(true);
+
+    auto buttonGroup = guiGroup->get("Buttons");
+    if (const auto grp = dynamic_pointer_cast<tgui::Group>(buttonGroup))
+    {
+        int cnt = 0;
+        for (auto w : grp->getWidgets())
+        {
+            if (cnt == m_buttons.max_size())
+                break;
+            if (const auto btn = dynamic_pointer_cast<tgui::Button>(w))
+            {
+                m_buttons[cnt] = w;
+                cnt++;
+                std::string name = btn->getWidgetName().toStdString();
+
+                if (name == "Start")
+                    w->getSignal("Pressed").connect([&manager = m_gameStateManager] { manager->setState("MainState"); });
+               /* else if (name == "Settings")
+                    w->getSignal("Pressed").connect(
+                        [&manager = m_gameStateManager] { manager->setState("SettingsPaddleSetupState"); });*/
+                //btn->onPress([&manager = m_gameStateManager] { manager->setState("Settings"); });
+                else if (name == "Quit")
+                    w->getSignal("Pressed").connect([&game = m_game] { game->getWindow().close(); });
+            }
+        }
+        m_buttons[m_selectedButton]->setFocused(true);
+    }
+}
+
+void PaddleSetupState::update(float delta)
+{
+    PROFILE_FUNCTION();
+
+    m_game->getWindow().setView(m_view);
+
+    handleButtons();
+}
+
+void PaddleSetupState::draw()
+{
+    PROFILE_FUNCTION();
+}
+
+void PaddleSetupState::exit()
+{
+    PROFILE_FUNCTION();
+
+    m_isInit = false;
+    GameState::exit();
+}
+
+void PaddleSetupState::handleButtons()
+{
+    int prevSelected = m_selectedButton;
+    if (InputManager::getInstance().isActionJustPressed("down") ||
+        InputManager::getInstance().isActionJustPressed(
+            "rig"
+            "h"
+            "t"))
+        m_selectedButton++;
+    else if (InputManager::getInstance().isActionJustPressed("up") ||
+             InputManager::getInstance().isActionJustPressed("left"))
+        m_selectedButton--;
+    else if (InputManager::getInstance().isActionJustPressed("switch"))
+    {
+        auto widget = m_buttons[m_selectedButton];
+        widget->getSignal("Pressed").emit(widget.get());
+    }
+
+    if (prevSelected != m_selectedButton)
+    {
+        m_selectedButton = m_selectedButton < 0 ? m_buttons.size() - 1 : m_selectedButton % m_buttons.size();
+        m_buttons[prevSelected]->setFocused(false);
+        m_buttons[m_selectedButton]->setFocused(true);
+    }
+}
+} // namespace mmt_gd
---
+++ src/PaddleSetupState.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "GameState.hpp"
+
+#include "TGUI/TGUI.hpp"
+#include <SFML/Graphics/Text.hpp>
+
+namespace mmt_gd
+{
+class PaddleSetupState final : public GameState
+{
+public:
+    using GameState::GameState;
+
+    void init() override;
+    void initGui() override { };
+
+    void update(float delta) override;
+    void draw() override;
+    void exit() override;
+
+private:
+    void handleButtons();
+
+    sf::View m_view;
+};
+} // namespace mmt_gd
