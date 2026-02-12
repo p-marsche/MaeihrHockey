91e5dccb877f5125c95cab5fc4756e4b45d3143d
Pascal Roland Veselka <fhs52324@fh-salzburg.ac.at>
2026-02-12 19:43:02 +0100

    made inactive pucks with no bounce component "static"

--- src/NoBouncePassive.cpp
+++ src/NoBouncePassive.cpp
@@ -3,10 +3,12 @@
 #include "IPlayerPassiveComponent.hpp"
 #include "GameObject.hpp"
 #include "NoBouncePassive.hpp"
+#include "RigidBodyComponent.hpp"
 
 namespace mmt_gd
 {
 float constexpr BASE_FACTOR = 0.0f;
+float constexpr EXTREME_DAMPENING = 1000.0f;
 
     NoBouncePassive::NoBouncePassive(GameObject& go) 
     : IPlayerPassiveComponent(go, PaddlePassive::TRAP)
@@ -14,9 +16,20 @@ float constexpr BASE_FACTOR = 0.0f;
     {
     }
     
+    void NoBouncePassive::update(float deltaTime)
+    {
+        if (m_enabled)
+        {
+            auto body1 = m_gameObject.getComponent<RigidBodyComponent>()->getB2Body();
+            body1->SetLinearDamping(EXTREME_DAMPENING);
+        }
+    }
+
     void NoBouncePassive::apply(b2Contact& contact)
     {
         if (m_enabled)
+        {
             contact.SetRestitution(m_bounceFactor);
+        }
     }
-}
+} 
--- src/NoBouncePassive.hpp
+++ src/NoBouncePassive.hpp
@@ -9,6 +9,8 @@ class NoBouncePassive final : public IPlayerPassiveComponent
 public:
     NoBouncePassive(GameObject& go);
 
+    void virtual update(float deltaTime) override;
+
     void virtual apply(b2Contact& contact) override;
 
     float m_bounceFactor;
